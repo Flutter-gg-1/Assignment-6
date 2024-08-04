b674c9d7d0243d9cc6f23a78823eeb12538c18c5
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-05 01:17:07 +0300

    added Inventory Class and its methods + started main method

--- edi_system/bin/edi_system.dart
+++ edi_system/bin/edi_system.dart
@@ -1,5 +1,39 @@
-
+import 'smartphone.dart';
+import 'laptop.dart';
+import 'inventory.dart';
 void main(List<String> arguments) {
+
+  Smartphone iphone15 = Smartphone(brand: 'Apple', model: 'iPhone 15 Pro', batteryLife: 23);
+  Smartphone s24 = Smartphone(brand: 'Samsung', model: 'S24 ultra', batteryLife: 25);
+  Smartphone pixel8Pro = Smartphone(brand: 'Google', model: 'Pixel 8 Pro', batteryLife: 24);
+
+  Laptop matebook14 = Laptop(brand: 'Huawei', model: 'MateBook 14', ramSize: 16);
+  Laptop macbookPro14 = Laptop(brand: 'Apple', model: 'MacBook 14 Pro', ramSize: 16);
+  Laptop pavilion15 = Laptop(brand: 'HP', model: 'Pavilion 15', ramSize: 16);
+
+  // Created Object from Invetory because it acts like cart (better than making it static)
+  Inventory inv = Inventory();
+
+  // Add Devices to inventory object
+  inv.addDevice(iphone15);
+  inv.addDevice(s24);
+  inv.addDevice(pixel8Pro);
+  inv.addDevice(matebook14);
+  inv.addDevice(macbookPro14);
+  inv.addDevice(pavilion15);
+
+  // Remove Device from inventory object
+  inv.removeDevice(pavilion15.model);
+
+  // Count number of Devices in inventory
+  print('Inventory contains ${inv.getDeviceCount()} Devices');
+
+  
+
+
+
+
+
 
 
 
---
+++ edi_system/bin/inventory.dart
@@ -0,0 +1,28 @@
+import 'electronic_device.dart';
+
+class Inventory{
+
+  List<ElectronicDevice> electronicDevices = [];
+  
+  addDevice(ElectronicDevice device){
+    electronicDevices.add(device);
+  }
+
+  void removeDevice(String model){
+    for (var i = 0; i < electronicDevices.length; i++) {
+      if(electronicDevices[i].model == model){
+        electronicDevices.removeAt(i);
+      }
+    }
+  }
+
+  int getDeviceCount(){
+    return electronicDevices.length;
+  }
+
+  displayDevices(){
+    for (var i = 0; i < electronicDevices.length; i++) {
+      print('${electronicDevices[i].displayDetails}\n');
+    }
+  }
+}
