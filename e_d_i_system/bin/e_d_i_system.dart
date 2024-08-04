5536e62d0f8795607b30b87ca5084e3551a4c207
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-04 18:35:27 +0300

    Hello, World

--- e_d_i_system/bin/e_d_i_system.dart
+++ e_d_i_system/bin/e_d_i_system.dart
@@ -1,5 +1,4 @@
-import 'package:e_d_i_system/e_d_i_system.dart' as e_d_i_system;
 
-void main(List<String> arguments) {
-  print('Hello world: ${e_d_i_system.calculate()}!');
+void main() {
+  print('Hello, World!');
 }
--- e_d_i_system/test/e_d_i_system_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:e_d_i_system/e_d_i_system.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
