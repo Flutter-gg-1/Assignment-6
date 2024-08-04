58f1a76e6eca95d609c86df81b1c2891d3b24832
yara albouq <yaramalbouq@gmail.com>
2024-08-04 20:45:42 +0300

    create displayDevices method

--- electronic_device_inventory_system/bin/inventory_class.dart
+++ electronic_device_inventory_system/bin/inventory_class.dart
@@ -2,21 +2,33 @@ import 'electronic_device.dart';
 
 class Inventory {
   List<ElectronicDevice> devices = [];
-  //Add a device to the inventory.
+  //method for Add a device to the inventory.
   addDevice({required ElectronicDevice device}) {
     devices.add(device);
   }
 
-//Remove a device from the inventory by model.
+//Method for remove a device from the inventory by model.
   removeDevice({required String deviceModel}) {
     for (var element in devices) {
       if (element.model == deviceModel) {
         devices.remove(element);
+      } else {
+        print("XXX Invalid device model XXX");
       }
     }
   }
 
 //Count the total number of devices in the inventory.
 
-//Display details of all devices in the inventory.
+//Method for display details of all devices in the inventory.
+  displayDevices() {
+    print("           All devices in inventory           ");
+    print("**********************************************");
+    print("\n");
+    for (var element in devices) {
+      element.displayDetails();
+      print("___________________________________________");
+      print("\n");
+    }
+  }
 }
