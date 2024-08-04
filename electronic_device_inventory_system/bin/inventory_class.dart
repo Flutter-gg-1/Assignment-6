b1b68e8e8287ff48aebcdbe5b18f47ec67583dcd
yara albouq <yaramalbouq@gmail.com>
2024-08-04 20:27:55 +0300

    create addDevice and removeDevice methods

--- electronic_device_inventory_system/bin/inventory_class.dart
+++ electronic_device_inventory_system/bin/inventory_class.dart
@@ -1,3 +1,22 @@
-class Inventory{
-  
+import 'electronic_device.dart';
+
+class Inventory {
+  List<ElectronicDevice> devices = [];
+  //Add a device to the inventory.
+  addDevice({required ElectronicDevice device}) {
+    devices.add(device);
+  }
+
+//Remove a device from the inventory by model.
+  removeDevice({required String deviceModel}) {
+    for (var element in devices) {
+      if (element.model == deviceModel) {
+        devices.remove(element);
+      }
+    }
+  }
+
+//Count the total number of devices in the inventory.
+
+//Display details of all devices in the inventory.
 }
