e350abbbc5a3a64289f252f85e5a5e0aa8955fbe
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-08-04 17:54:10 +0300

    create dart project to build Dlectronic Device Inventory system

---
+++ electronic_evice_inventory_system/test/electronic_evice_inventory_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:electronic_evice_inventory_system/electronic_evice_inventory_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
