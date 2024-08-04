d0ba4beeaead18f4c114cbf0396b34473af3c78f
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-04 20:13:22 +0300

    Add ElectronicDevice

---
+++ e_d_i_system/bin/electronic_device.dart
@@ -0,0 +1,52 @@
+class ElectronicDevice {
+  String? brand;
+  String? model;
+
+  void displayDetails() {
+    print('Brand: $brand');
+    print('Model: $model');
+  }
+
+  bool compareModel(ElectronicDevice other) {
+    return model == other.model;
+  }
+}
+
+class Smartphone extends ElectronicDevice {
+  int? batteryLife; // in hours
+  Smartphone({String? brand, String? model, this.batteryLife}) : super();
+
+  @override
+  void displayDetails() {
+    super.displayDetails();
+    print('Battery life: $batteryLife hours');
+  }
+}
+
+class Laptop extends ElectronicDevice {
+  int? ramSize; // in GB
+  Laptop({String? brand, String? model, this.ramSize}) : super();
+  @override
+  void displayDetails() {
+    super.displayDetails();
+    print('RAM size: $ramSize GB');
+  }
+}
+class Inventory{
+  List<ElectronicDevice> devices = [];
+
+  void addDevice(ElectronicDevice device) {
+    devices.add(device);
+  }
+
+  void removeDevice(ElectronicDevice device) {
+    devices.remove(device);
+  }
+  void countTotalNumberOfDevices(){
+    print('Total number of devices: ${devices.length}');}
+  void displayDevices() {
+    for (var device in devices) {
+      device.displayDetails();
+    }
+  }
+}
