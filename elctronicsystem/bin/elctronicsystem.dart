723eb512de2bd6df76421dd6ad16c258370194bc
muhannad <mohnsalzaher@gmail.com>
2024-08-05 01:10:52 +0300

    app tested and working fine

--- elctronicsystem/bin/elctronicsystem.dart
+++ elctronicsystem/bin/elctronicsystem.dart
@@ -8,10 +8,17 @@ void main(List<String> arguments) {
       smartPhone(brand: "apple", model: "iphone 15 pro", batteryLife: 13);
   var samsung = smartPhone(brand: "Samsung", model: "g7", batteryLife: 12);
   var laptop = Laptop(brand: "assus", model: "windows", ramSize: 16);
+   print("______________________Compare Divce_________________________________________");
+  iphone.compareModel(model: "Iphone 15 pro", currentModel: "g7");
+  print("____________________Add divce_________________________________________");
   var infontry = Inventory();
   infontry.addDevices(devic: iphone);
   infontry.addDevices(devic: samsung);
   infontry.addDevices(devic: laptop);
+  infontry.displayAllDevices();
+  infontry.divicesCount();
+  infontry.removeDevices(model: "g7");
+  print("_______________________Remove divce_________________________________________");
   infontry.displayAllDevices();
   infontry.divicesCount();
 }
--- elctronicsystem/modules/inventory.dart
+++ elctronicsystem/modules/inventory.dart
@@ -1,24 +1,22 @@
-
-
 import 'electronicDevice.dart';
 
 class Inventory {
   List<Electronicdevice> devices = [];
 
   static int count = 0;
- 
-
-
 
   addDevices({required Electronicdevice devic}) {
     devices.add(devic);
     count += 1;
   }
-  divicesCount() {
-    print(count);
-  }
+
   removeDevices({required String model}) {
     devices.removeWhere((device) => device.model == model);
+    count = count-1;
+  }
+
+  divicesCount() {
+    print(count);
   }
 
   displayAllDevices() {
