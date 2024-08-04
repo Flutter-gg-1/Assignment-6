e9776d5a1554bd6824c28a7e35fc421b76e7d07f
turki-aloufi <turkialoufi44@gmail.com>
2024-08-04 22:05:34 +0300

    create Inventory class

---
+++ inventory_system/bin/inventory.dart
@@ -0,0 +1,23 @@
+import 'electronic_device.dart';
+
+class Inventory {
+  static List<ElectronicDevice> devices = [];
+
+  static void addDevice(ElectronicDevice device) {
+    devices.add(device);
+  }
+
+  static void removeDevice(ElectronicDevice device) {
+    devices.remove(device);
+  }
+
+  static int getNumberOfDevices() {
+    return devices.length;
+  }
+
+  static void displayDetails() {
+    for (var device in devices) {
+      device.displayDetails();
+    }
+  }
+}
