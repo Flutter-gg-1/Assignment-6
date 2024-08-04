510c959f99c1be57898ffe6c2dc6b7474ccfe8e5
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-08-04 23:44:32 +0300

    added dart project edi_system + class ElectronicDevice and its methods

---
+++ edi_system/bin/electronic_device.dart
@@ -0,0 +1,15 @@
+class ElectronicDevice{
+  String brand;
+  String model;
+
+  ElectronicDevice({required this.brand, required this.model});
+
+  void displayDetails(){
+    print('Brand: $brand');
+    print('Model: $model');
+  }
+
+  bool compareModel(ElectronicDevice other){
+    return model == other.model;
+  }
+}
