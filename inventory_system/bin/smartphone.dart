e3f4899fe18897540a561a3270f9c6b2edabbf38
turki-aloufi <turkialoufi44@gmail.com>
2024-08-04 22:04:34 +0300

    create Smartphone class

---
+++ inventory_system/bin/smartphone.dart
@@ -0,0 +1,16 @@
+import 'electronic_device.dart';
+
+class Smartphone extends ElectronicDevice {
+  int? batteryLife;
+
+  Smartphone({required super.brand, required super.model, this.batteryLife});
+
+  @override
+  void displayDetails() {
+    print("\n\n----------Device details----------");
+    print("Brand: $brand");
+    print("Model: $model");
+    print("Battery life: $batteryLife");
+    print("----------------------------------");
+  }
+}
