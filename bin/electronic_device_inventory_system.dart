50da83117b09e52fbe315143693f85b52f1a195f
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-04 19:18:05 +0300

    invantory class was created

--- bin/electronic_device_inventory_system.dart
+++ bin/electronic_device_inventory_system.dart
@@ -1,4 +1,5 @@
+import 'package:electronic_device_inventory_system/laptop.dart';
+import 'package:electronic_device_inventory_system/smart_phone.dart';
+import 'package:electronic_device_inventory_system/inventory.dart';
 
-void main(List<String> arguments) {
-
-}
+void main(List<String> arguments) {}
---
+++ lib/inventory.dart
@@ -0,0 +1,30 @@
+class Inventory {
+  List devices = [];
+  static int count = 0;
+  Inventory({required this.devices});
+  void addDevice(var device) {
+    devices.add(device);
+    count++;
+  }
+
+  void removeDevice(String model) {
+    int length = count;
+    devices.removeWhere((device) => device.model == model);
+    if (length > devices.length) {
+      count--;
+      print("delete was success");
+    } else {
+      print("device does not exitst");
+    }
+  }
+
+  void displyaAllDecvices() {
+    for (var device in devices) {
+      device.displayDetails();
+    }
+  }
+
+  int noOfDevices() {
+    return count;
+  }
+}
