9d45bcc7a381853b8fea5686f657d5545df8c1a0
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-04 18:19:44 +0300

    class smart phone was created

---
+++ lib/smart_phone.dart
@@ -0,0 +1,13 @@
+import 'package:electronic_device_inventory_system/electronic_device.dart';
+
+class SmartPhone extends ElectronicDevice {
+  late int batteryLife;
+
+  SmartPhone(
+      {required super.brand, required super.model, required this.batteryLife});
+
+  @override
+  displayDetails() {
+    print("Brand: $brand\nModel: $model\nBattry life: $batteryLife");
+  }
+}
