8383a0cc39eaebb4bff42021977aad79cf996b6b
anas <anas.aljuhani.dev@gmail.com>
2024-08-05 00:00:47 +0300

    laptop class created

---
+++ electronicsys/bin/laptop.dart
@@ -0,0 +1,12 @@
+import 'electronic_device.dart';
+
+class LapTop extends ElectronicDevice {
+  int? ramSize;
+  LapTop({required super.brand, required super.model, required this.ramSize});
+
+  @override
+  void displayDetails() {
+    super.displayDetails();
+    print('Ram Size: $ramSize');
+  }
+}
